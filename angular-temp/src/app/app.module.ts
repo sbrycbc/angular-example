9f56fcb4eb411747c0b51e17c6ebb38465848510
sbrycbc <sabriyecbc@gmail.com>
2024-09-20 14:52:38 +0200

    added heder component...

---
+++ angular-temp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+   AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent] 
+})
+export class AppModule { }
